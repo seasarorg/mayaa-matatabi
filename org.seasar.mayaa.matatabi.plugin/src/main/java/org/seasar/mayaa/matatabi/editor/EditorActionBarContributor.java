0ea2bb4c8261798f4516caa7b1b51f5e0d829e82
matobat <matobat@gmail.com>
2008-02-01 00:30:34 +0000

    https://www.seasar.org/issues/browse/MAYAA-43対応
    git-svn-id: https://www.seasar.org/svn/mayaa/trunk/matatabi@3255 64d2bceb-c1f7-0310-a597-f30c3359d46a

---
+++ org.seasar.mayaa.matatabi.plugin/src/main/java/org/seasar/mayaa/matatabi/editor/EditorActionBarContributor.java
@@ -0,0 +1,7 @@
+package org.seasar.mayaa.matatabi.editor;
+
+import org.eclipse.wst.xml.ui.internal.actions.ActionContributorXML;
+
+public class EditorActionBarContributor extends ActionContributorXML {
+	
+}
